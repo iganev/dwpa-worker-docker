18195b4ae2c2ec126661e54538234ad49d957a1e
Ivan Ganev <iganev@cytec.bg>
2024-09-26 17:03:05 -0600

    wip

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,9 +3,9 @@
 RUNTIME_DEST=/srv/app/runtime
 
 # Download dwpa hc runtime
-wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}hc/help_crack.py"
-wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}hc/help_crack.py.version"
-wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}hc/CHANGELOG"
+wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}/hc/help_crack.py"
+wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}/hc/help_crack.py.version"
+wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}/hc/CHANGELOG"
 
 chmod +x "${RUNTIME_DEST}/help_crack.py"
 

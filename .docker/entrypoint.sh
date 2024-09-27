2482e49fe50bb250bcbabb65ab065e561a68be35
Ivan Ganev <iganev@cytec.bg>
2024-09-27 16:31:03 -0600

    wip

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -11,4 +11,6 @@ chmod +x "${RUNTIME_DEST}/help_crack.py"
 
 AD="" # -ad=""
 
-cd $RUNTIME_DEST && screen -S help_crack -L -Logfile help_crack.log ./help_crack.py -co="$COPTIONS" -pot=results.pot $AD
+# screen -S help_crack -L -Logfile help_crack.log ./help_crack.py -co="$COPTIONS" -pot=results.pot $AD
+
+cd $RUNTIME_DEST && ./help_crack.py -co="$COPTIONS" -pot=results.pot $AD

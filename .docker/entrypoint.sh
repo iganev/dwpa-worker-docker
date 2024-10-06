3c21b82a2166d41a37dc004b9c1d4d1e8209d450
Ivan Ganev <iganev@cytec.bg>
2024-10-05 18:43:51 -0600

    wip

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -9,7 +9,17 @@ wget -nc -P $RUNTIME_DEST "${PUBLIC_URL}/hc/CHANGELOG"
 
 chmod +x "${RUNTIME_DEST}/help_crack.py"
 
-AD="" # -ad=""
+for RULE in $RULES
+do
+  COPTIONS+=" -r ../rules/${RULE}"
+done
+
+if [ ! -z "$EXTRA_DICT" ]
+then
+  AD="-ad=\"../dicts/${EXTRA_DICT}\""
+else
+  AD=""
+fi
 
 # screen -S help_crack -L -Logfile help_crack.log ./help_crack.py -co="$COPTIONS" -pot=results.pot $AD
 
